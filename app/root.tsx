3814f27dbc7ff99e0d3c770df7f2bff56b328c6b
Antonella Capadona <antocapadona7@gmail.com>
2024-02-19 23:02:44 -0300

    'tutorial finalizado'

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,11 @@
-import type { LinksFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunctionArgs,
+} from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
-  Link,
+  NavLink,
   Links,
   LiveReload,
   Meta,
@@ -10,22 +13,49 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
+  useSubmit,
 } from "@remix-run/react";
+import { useEffect } from "react";
+
 
 import appStylesHref from "./app.css";
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
-  return json({ contacts });
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts,q });
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+  	navigation.location &&
+  	new URLSearchParams(navigation.location.search).has(
+   	 "q"
+ 	);
+    
+    useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <html lang="en">
@@ -39,15 +69,29 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form 
+            	id="search-form" 
+            	onChange={(event) => {
+                	const isFirstSearch = q === null;
+                	submit(event.currentTarget, {
+                  		replace: !isFirstSearch,
+                	});
+              }} 
+                role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div     
+              	aria-hidden
+                hidden={!searching}
+                id="search-spinner" 
+              />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
@@ -58,7 +102,16 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "active"
+                      : isPending
+                      ? "pending"
+                      : ""
+                  }
+                  to={`contacts/${contact.id}`}
+                >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -69,7 +122,7 @@ export default function App() {
                       {contact.favorite ? (
                         <span>★</span>
                       ) : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -80,7 +133,13 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div 
+           className={
+            navigation.state === "loading" && !searching 
+            ? "loading" 
+            : ""
+          }
+          id="detail">
           <Outlet />
         </div>
 
